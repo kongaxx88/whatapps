4f049a57bee763c32de0a90ebaad130f8163f8e6
kongaxx88 <121180500+kongaxx88@users.noreply.github.com>
2023-01-08 12:50:49 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ from time import sleep
 import pyperclip
 import random
 
-sleep(3)
-
-position1 = pt.locateOnScreen("smiley_paperclip.png")
-#position1 = pt.locateOnScreen(r"C:\Users\ngkel\PycharmProjects\whatappsbot_yt\whatsapp\smiley_paperclip.png")
-#, confidence=.6)
-#position1 is none
+#it is working now, you just need to ensure your image is shown on the screen
+position1 = pt.locateOnScreen(r"C:\Users\ngkel\PycharmProjects\whatappsbot_yt\whatsapp\smiley_paperclip.png", confidence=.3)
 print(position1)
-x = position1[0]
-y = position1[1]
+
+sleep(3)
+print(position1)
+# x = position1[0]
+# y = position1[1]
+#print(x)
+
+if position1 == None:
+    print("image not found")
+else:
+    x1 = position1[0]
+    y1 = position1[1]
+    #print(x1)
 
 
-# get message
 def get_message():
     global x, y
 
-    position = pt.locateOnScreen(r"C:\Users\ngkel\PycharmProjects\whatappsbot_yt\whatsapp\smiley_paperclip.png", confidence=.6)
+    position = pt.locateOnScreen(r"C:\Users\ngkel\PycharmProjects\whatappsbot_yt\whatsapp\smiley_paperclip.png", confidence=.3)
     x = position[0]
     y = position[1]
     pt.moveTo(x, y, duration=0.5)
@@ -32,19 +38,18 @@ def get_message():
 
     return whatsapp_message
 
-# #Posts
-# def post_response(message)
-#     global x,y
-#
-#     position = pt.locateOnScreen("whatsapp/smiley_paperclip.png", confidence=.6)
-#     x=position[0]
-#     y=position[1]
-#     pt.moveTo(x+200, y+20, duration=0.5)
-#     pt.click()
-#     pt.typewrite(message, interval=0.01)
-#
-#     pt.typewrite("\n", interval=0.01)
-#
-# get_message()
-#
-# post_response()
+#Post
+def post_response(message):
+    global x,y
+
+    position = pt.locateOnScreen(r"C:\Users\ngkel\PycharmProjects\whatappsbot_yt\whatsapp\smiley_paperclip.png", confidence=.3)
+    x=position[0]
+    y=position[1]
+    pt.moveTo(x+200, y+20, duration=0.5)
+    pt.click()
+    pt.typewrite(message, interval=0.01)
+
+    pt.typewrite("\n", interval=0.01)
+
+
+post_response(get_message())
